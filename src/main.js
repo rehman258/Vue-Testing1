12f104f26427bf3c9f894c164c4b8ef73cd29690
Rahman Aliyev <rahman.aliyev@mpay.az>
2023-07-28 16:40:01 +0400

    testCommit

--- src/main.js
+++ src/main.js
@@ -2,9 +2,11 @@ import Vue from 'vue'
 import App from './App.vue'
 
 import store from './store';
+import vuetify from './plugins/vuetify'
 
 new Vue({
   el: '#app',
   store,
+  vuetify,
   render: h => h(App)
 })
